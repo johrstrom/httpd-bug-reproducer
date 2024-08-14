41dcb145fb7a1e2ae09333667cc1c018a7def36f
Jeff Ohrstrom <johrstrom@osc.edu>
2024-08-14 13:11:46 -0400

    add newlines to help output formatting

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ buildah bud -t $GOOD_IMG -f Dockerfile.works .
 podman run --rm --name=$GOOD_NAME -p 8080:8080 -d $GOOD_IMG
 timeout 3 curl localhost:8080/test || true
 
+echo  ""
 echo "======= logs from the version that works ==========="
 podman logs $GOOD_NAME
 podman stop $GOOD_NAME
@@ -21,6 +22,7 @@ buildah bud -t $BAD_IMG -f Dockerfile.broken .
 podman run --rm --name=$BAD_NAME -p 8080:8080 -d $BAD_IMG
 timeout 3 curl localhost:8080/test || true
 
+echo  ""
 echo "======= logs from the version that's broken ==========="
 podman logs $BAD_NAME
 podman stop $BAD_NAME
